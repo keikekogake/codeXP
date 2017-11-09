5ffaf63aed2626a233394cea46b7e0cb65de355b
keikekogake <keike.kogake@gmail.com>
2017-11-08 22:00:48 -0200

    Criação da classe Show.cs

---
+++ ProjetoEvento/ClassePai/ClassesFilhas/Show.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace ProjetoEvento.ClassePai.ClassesFilhas {
+    public class Show : Evento {
+        public string Artista { get; set; }
+        public string GeneroMusical { get; set; }
+
+        public Show () { }
+
+        public Show (string titulo, string local, int lotacao, string duracao, int classificacao, DateTime data, string artista, string generomusical) {
+            base.Titulo = titulo;
+            base.Local = local;
+            base.Lotacao = lotacao;
+            base.Duracao = duracao;
+            base.Classificacao = classificacao;
+            base.Data = data;
+            this.Artista = artista;
+            this.GeneroMusical = generomusical;
+        }
+
+        public override bool Cadastrar () { 
+            return false;
+        }
+    }
+}
