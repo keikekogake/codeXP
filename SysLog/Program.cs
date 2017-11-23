773d02dd2d7571326face9c533b8ba7f113545fe
keikekogake <keike.kogake@gmail.com>
2017-11-23 21:59:44 -0200

    Aula Eventos

---
+++ SysLog/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SysLog {
+    class Program {
+        static void Main (string[] args) {
+            string op = "";
+            string usuario = "";
+            string senha = "";
+            Usuario us = new Usuario ();
+
+            do {
+                Console.WriteLine ("Escolha uma das opções");
+                Console.WriteLine ("1 - Cadastro\n2 - Logar\n9 - Sair");
+                op = Console.ReadLine ();
+
+                switch (op) {
+                    case "1":
+                        Console.Write ("Digite o usuário: ");
+                        usuario = Console.ReadLine ();
+                        Console.Write ("Digite a senha: ");
+                        senha = Console.ReadLine ();
+                        Console.WriteLine (us.Cadastrar (usuario, senha));
+                        break;
+
+                    case "2":
+                        Console.Write ("Digite o usuário: ");
+                        usuario = Console.ReadLine ();
+                        Console.Write ("Digite a senha: ");
+                        senha = Console.ReadLine ();
+                        Console.WriteLine (us.Logar (usuario, senha));
+
+                        break;
+
+                    default:
+                        break;
+                }
+            } while ((op) != "9");
+        }
+    }
+}
---
+++ SysLog/Usuario.cs
@@ -0,0 +1,70 @@
+using System;
+using System.IO;
+using System.Text;
+
+namespace SysLog {
+    public class Usuario : IUsuario {
+        public string User { get; set; }
+        public string Senha { get; set; }
+
+        public Usuario () { }
+
+        public Usuario (string user, string senha) {
+            this.User = user;
+            this.Senha = senha;
+        }
+        public string Cadastrar (string usuario, string senha) {
+            string msg = "";
+            StreamWriter sw = null;
+
+            try {
+                sw = new StreamWriter ("_usuario.txt", true);
+                sw.WriteLine (usuario + ";" + senha);
+                msg = "Usuario cadastrado";
+            } catch (Exception ex) {
+                msg = "Erro ao manipular o arquivo: " + ex.Message;
+            } finally {
+                sw.Close ();
+            }
+
+            return msg;
+        }
+
+        public string Logar (string usuarioEntrada, string senhaEntrada) {
+            string[] array;
+            string msg = "";
+            string linha = "";
+            StreamReader sr = null;
+
+            try {
+                sr = new StreamReader ("_usuario.txt", Encoding.Default);
+                while ((linha = sr.ReadLine ()) != null) {
+                    array = linha.Split (';');
+                    if (usuarioEntrada == array[0] && senhaEntrada == array[1]) {
+                        msg = "Usuário válido";
+
+                    }
+                }
+            } catch (Exception ex) {
+                msg = "Erro ao manipular o arquivo" + ex.Message;
+            } finally {
+                sr.Close ();
+            }
+            return msg;
+        }
+
+        public void Superior () {
+
+        }
+
+        public delegate string DLogar (string usuario, string senha);
+        public event DLogar ELogar;
+
+        public string Logar () {
+            throw new System.NotImplementedException ();
+        }
+        public string Cadastrar () {
+            throw new NotImplementedException ();
+        }
+    }
+}
