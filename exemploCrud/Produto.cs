f4b4a258dcbd913e846b8177306960f9451abf96
keikekogake <keike.kogake@gmail.com>
2017-12-05 21:55:51 -0200

    Exemplo Banco de dados

---
+++ exemploCrud/Produto.cs
@@ -0,0 +1,7 @@
+namespace exemploCrud
+{
+    public class Produto
+    {
+        
+    }
+}
---
+++ exemploCrud/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace exemploCrud {
+    class Program {
+        static void Main (string[] args) {
+
+        }
+    }
+}
