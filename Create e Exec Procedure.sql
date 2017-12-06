55ec5caa6f6a67c868b368d812b0c4d57d17a2ce
keikekogake <keike.kogake@gmail.com>
2017-12-06 21:46:59 -0200

    Aula Procedure

---
+++ Create e Exec Procedure.sql
@@ -0,0 +1,12 @@
+select * from Cliente
+
+CREATE PROCEDURE sp_CadCliente
+@nome varchar (50),
+@email varchar(100),
+@cpf varchar(20)
+as
+INSERT INTO Cliente (nome_cliente, email_cliente, cpf_cliente)
+VALUES (@nome, @email, @cpf)
+
+
+EXEC sp_CadCliente 'Keike','keike.kogake@gmail.com','40314256865'
